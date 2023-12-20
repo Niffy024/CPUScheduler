3dcdf54e78c0ccccaed0088f45a73878d11fb638
niffy <niffyjay2001@gmail.com>
2023-12-20 10:32:31 +0200

    first commit

---
+++ CpuScheduler.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+
+int main(){
+    cout<<"Welcome User, this is CPU Scheduler Simulator.Make sure you already add your input file"<<endl;
+
+
+
+
+
+    return 0;
+}
