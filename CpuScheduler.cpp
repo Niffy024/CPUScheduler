1ae60e2bb2fbdd7d059897805bde2f2529ae8abf
niffy <niffyjay2001@gmail.com>
2023-12-24 23:13:36 +0200

    I implemented linked list with  structure for each process and some process-related functions

--- CpuScheduler.cpp
+++ CpuScheduler.cpp
@@ -1,9 +1,95 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
+
+
+struct Process {
+    int burstTime;
+    int arrivalTime;
+    int priority;
+    int waitingTime;
+    float AvgWaitingTime;
+    Process* next;
+};
+
+void addProcess(Process*& head, Process*& tail, int burstTime, int arrivalTime, int priority) {
+    Process* newProcess = new Process;
+    newProcess->burstTime = burstTime;
+    newProcess->arrivalTime = arrivalTime;
+    newProcess->priority = priority;
+    newProcess->next = nullptr;
+
+    if (head == nullptr) {
+        head = tail = newProcess;
+    } else {
+        tail->next = newProcess;
+        tail = newProcess;
+    }
+}
+
+//use this printProcesses function to calculate and get each cpu scheduling method
+void printProcesses(const Process* head) {
+    const Process* current = head;
+        int count=1;
+        ofstream outFile("out.txt");
+       
+    while (current != nullptr) {
+               
+                
+        
+     
+        cout << "P"<< count<< ":"<<"Burst Time: " << current->burstTime << ", Arrival Time: " << current->arrivalTime
+             << ", Priority: " << current->priority << endl; 
+            
+        //current = current->next;
+        outFile<<"P"<< count<< ":"<<"Burst Time: " << current->burstTime << ", Arrival Time: " << current->arrivalTime
+             << ", Priority: " << current->priority << endl; 
+            
+        current = current->next;
+         count++;
+          
+    }
+   
+     outFile.close();
+}
+
 
 int main()
 {
+
+
+
+     ifstream inputFile("input.txt");
+    if (inputFile.fail()) {
+        cerr << "Error: Unable to open input file." << endl;
+        return 1;
+    }
+
+    Process* head = nullptr;
+    Process* tail = nullptr;
+    string line;
+
+    while (getline(inputFile, line)) {
+        stringstream ss(line);
+        int burstTime, arrivalTime, priority;
+        char colon;
+
+        ss >> burstTime >> colon >> arrivalTime >> colon >> priority;
+
+        addProcess(head, tail, burstTime, arrivalTime, priority);
+    }
+
+    inputFile.close();
+     
+     
+    // Output the assigned values
+     
+     
+   
+
+
+
     cout << "Welcome User, this is CPU Scheduler Simulator.Make sure you already add your input file" << endl;
     cout << "1. Scheduling Method" << endl;
     cout << "2. Preemptive Mode" << endl;
@@ -25,6 +111,12 @@ int main()
         cout << "-----------------5. Round Robin Scheduling--------------------------" << endl;
         cout << "Choose your Sub Option" << endl;
         cin >> subOption1;
+        if(subOption1==1){
+             cout << "Assigned values: " << endl;
+    printProcesses(head);
+        }
+       //add  printProcesses(head); functions for each scenerio here
+
         break;
         // Preemptive Method
     case 2:
@@ -48,6 +140,11 @@ int main()
         // cout << "Please choose from the Options listed!!";
         break;
     }
-
+ // Cleanup: free allocated memory
+    while (head != nullptr) {
+        Process* temp = head;
+        head = head->next;
+        delete temp;
+    }
     return 0;
 }
